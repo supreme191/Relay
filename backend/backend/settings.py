c464fc62bd2b66764b89d8a4c6e8665ab6e4073d
Saurabh Desai <saurabhdesai19121@gmail.com>
2026-08-13 07:53:33 +0530

    ADD: Models and Database Configuration.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'relay',
+        'HOST' : 'localhost',
+        'USER' : 'root',
+        # Enter your username and password here (Based on the credentials from MySQL server.)
+        'PASSWORD' : 'password',
     }
 }
 
@@ -127,3 +131,5 @@ MAILERS = {
         'BACKEND': 'django.core.mail.backends.console.EmailBackend',
     },
 }
+
+AUTH_USER_MODEL = "api.User"
