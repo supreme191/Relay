9794e7a4ef6d6225076081eccb3bc268400f2857
Saurabh Desai <saurabhdesai19121@gmail.com>
2026-08-25 02:29:06 +0530

    ADD: Protection to Chat route.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Chat from "./pages/Chat";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
     return (
@@ -10,7 +11,14 @@ function App() {
             <Routes>
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
-                <Route path="/chat" element={<Chat />} />
+                <Route
+                    path="/chat"
+                    element={
+                        <ProtectedRoute>
+                            <Chat />
+                        </ProtectedRoute>
+                    }
+                />
 
                 <Route
                     path="*"
